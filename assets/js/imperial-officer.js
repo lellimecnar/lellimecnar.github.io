576b76fd885153ed19406c61ae8eee432269a94a
Lance Miller <github@lance.pub>
2017-11-25 23:16:16 -0700

    Update imperial-officer.js

--- assets/js/imperial-officer.js
+++ assets/js/imperial-officer.js
@@ -1,1 +1,19 @@
-console.log('Imperial Officer JS loaded');
+(function () {
+	var rankBars = document.querySelectorAll('i[data-rank-bar]');
+	
+	[].forEach.call(rankBars, function(bar) {
+		var html = '';
+		var rows = bar.dataset.rankBar.split('-');
+
+		rows.forEach(function(row) {
+			var tiles = row.split('');
+			html += '<span>';
+			
+			tiles.forEach(function(tileColor) {
+				html += '<i data-rank-tile="' + tileColor + '"></i>';
+			});
+			
+			html += '</span>';
+		});
+	});
+})();
