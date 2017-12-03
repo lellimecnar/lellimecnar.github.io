304621e1c1ad647f307d40e81a4449a91ed7b5f4
Lance Miller <github@lance.pub>
2017-12-03 13:47:51 -0700

    Fix broken interaction

--- assets/js/imperial-officer.js
+++ assets/js/imperial-officer.js
@@ -2,7 +2,7 @@ $(document).on('click', '#FrontPiecesList li', function(e) {
 	var $this = $(this);
 	var pieceId = $this.find('strong').first().text();
 	var text = $this.clone().find('em').remove().end().html();
-	var $wrapper = $this.closest('#FrontPiecesList').siblings('.PatternPieces__frontTemplateWrapper');
+	var $wrapper = $this.closest('#FrontPiecesList').siblings('.PatternPieces__frontPiecesWrapper');
 
 	$wrapper
 		.attr('data-current', pieceId)
